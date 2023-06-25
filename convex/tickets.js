cd12617801988303fcb91aa73e4e3cf76d09f8fa
zac <zacchaeuschok2014@gmail.com>
2023-06-25 01:13:40 -0700

    Add convex

---
+++ convex/tickets.js
@@ -0,0 +1,19 @@
+import { query, mutation } from "./_generated/server";
+
+export const get = query(async ({ db }) => {
+  return await db.query("tickets").collect();
+});
+
+export const createTicket = mutation(async ({ db }, body) => {
+  const {
+    name = null,
+    description = null,
+    type = null,
+    acceptance_criteria = null,
+    how_to_reproduce = null
+  } = body;
+
+  const ticketId = await db.insert("tickets", { name, description, type, acceptance_criteria, how_to_reproduce });
+
+  return ticketId;
+});
--- server/app.py
+++ server/app.py
@@ -1,10 +1,30 @@
+import os
+from pprint import pprint
+
 import embeddings
 import raja
+from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 
+from convex import ConvexClient
+
 app = Flask("Raja")
 cors = CORS(app)
+
+# get the directory of the current script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# go up one level to get the root directory
+root_dir = os.path.dirname(current_dir)
+
+dotenv_path = os.path.join(root_dir, ".env.local")
+
+# load the .env file
+print(dotenv_path)
+load_dotenv(dotenv_path)
+
+client = ConvexClient(os.getenv("NEXT_PUBLIC_CONVEX_URL"))
 
 
 @app.route("/v1/initialize-repo", methods=["POST"])
@@ -27,6 +47,21 @@ def run_raja():
     print(req_data)
     raja.raja_agent(req_data)
     return jsonify(message="Raja workflow executed successfully"), 200
+
+
+@app.route("/v1/get-ticket", methods=["GET"])
+def get_tickets():
+    tickets = client.query("tickets:get")
+    pprint(tickets)
+    return tickets
+
+
+@app.route("/v1/create-ticket", methods=["POST"])
+def create_ticket():
+    req_data = request.get_json()
+    print(req_data)
+    client.mutation("tickets:createTicket", req_data)
+    return {}
 
 
 if __name__ == "__main__":
