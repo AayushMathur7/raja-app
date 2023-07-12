f4b1bed45f9eeeab82c08888df19ca154da21017
rajaverse <136966268+rajaverse@users.noreply.github.com>
2023-07-11 22:48:28 -0700

    Create status filter above tasktable component in tasktable.jsx

--- src/enums/StatusType.jsx
+++ src/enums/StatusType.jsx
@@ -2,4 +2,5 @@ export const StatusType = {
     READY_TO_DEPLOY: "ready_to_deploy",
     IN_PROGRESS: "in_progress",
     PR_READY_FOR_REVIEW: "pr_ready_for_review",
+    STATUS_FILTER: "status_filter", // Added status filter option
 }
